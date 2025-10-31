5907dc406d470f29bda61d09007c4e9e3f170fed
Somil Agrawal <somil.agarw@gmail.com>
2025-10-31 14:30:11 +0530

    fixed manifest

--- manifest_write.R
+++
@@ -1,9 +0,0 @@
-suppressPackageStartupMessages({
-  if (!requireNamespace('rsconnect', quietly = TRUE)) {
-    stop('Please install rsconnect: install.packages("rsconnect")')
-  }
-})
-
-rsconnect::writeManifest(appDir = '.')
-cat('Wrote manifest.json to project root.\n')
-
